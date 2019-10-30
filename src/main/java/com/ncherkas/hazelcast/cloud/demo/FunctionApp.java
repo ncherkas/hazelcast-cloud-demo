0add3d03e12dd9c86eb65eed18138cb435df1189
Nazarii <nazarii@hazelcast.com>
2019-10-30 17:28:07 +0200

    Tuned client timeouts

--- src/main/java/com/ncherkas/hazelcast/cloud/demo/FunctionApp.java
+++ src/main/java/com/ncherkas/hazelcast/cloud/demo/FunctionApp.java
@@ -20,16 +20,12 @@ public class FunctionApp {
 
 	@Bean
 	public HazelcastInstance hazelcastClient(Properties props) {
-		/**
-		 * Configure heartbeat timeout:
-		 * 2019-10-23 07:29:17.726 WARN 1 --- [nt_0.internal-2] c.h.c.connection.nio.HeartbeatManager : hz.client_0 [CloudDemo] [3.11.1] Heartbeat failed over the connection: ClientConnection{alive=true, connectionId=1, channel=NioChannel{/169.254.76.1:53567->/54.149.166.126:31802}, remoteEndpoint=[100.112.128.77]:31802, lastReadTime=2019-10-23 07:25:57.222, lastWriteTime=2019-10-23 07:25:57.142, closedTime=never, connected server version=3.12.2}
-		 */
-
 		System.out.println("Using: " + props);
 		Properties.HazelcastCloud hazelcastCloudProps = props.getHazelcastCloud();
 		ClientConfig config = new ClientConfig();
 		config.setGroupConfig(new GroupConfig(hazelcastCloudProps.getClusterName(), hazelcastCloudProps.getClusterPassword()));
 		config.setProperty("hazelcast.client.statistics.enabled", "true");
+		config.setProperty("hazelcast.client.heartbeat.timeout", "300000");
 		config.setProperty(ClientProperty.HAZELCAST_CLOUD_DISCOVERY_TOKEN.getName(), hazelcastCloudProps.getDiscoveryToken());
 		config.setProperty(HazelcastCloudDiscovery.CLOUD_URL_BASE_PROPERTY.getName(), hazelcastCloudProps.getUrlBase());
 		return HazelcastClient.newHazelcastClient(config);
